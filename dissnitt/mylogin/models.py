38422744b42533fec4480fdf84a36d431075d096
ciorg <charlie.iorg@infusionsoft.com>
2017-09-14 06:51:59 -0700

    fixed migration mixup and added notes index, and notes model

--- dissnitt/mylogin/models.py
+++ dissnitt/mylogin/models.py
@@ -1,18 +1,3 @@
 from django.db import models
 
-
-class Tag(models.Model):
-    name = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
-
-
-class Note(models.Model):
-    title = models.CharField(max_length=30)
-    body = models.TextField()
-    created_date = models.DateTimeField('date created')
-    tags = models.ManyToManyField(Tag)
-
-    def __str__(self):
-        return self.title
+# Create your models here.
--- dissnitt/notes/models.py
+++ dissnitt/notes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField()
+    created_date = models.DateTimeField('date created')
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
