6649421f9b377a77b734657d60e5c23b86233254
ciorg <charlie.iorg@infusionsoft.com>
2017-09-28 21:36:57 -0700

    figuring out the tag thing

--- dissnitt/notes/views.py
+++ dissnitt/notes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views import generic
+from django.views import generic, View
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Note, Tag
 from .forms import NoteForm, TagForm
+from django.urls import reverse
+from django.http import HttpResponseForbidden
+from django.views.generic.detail import SingleObjectMixin
+
 
 
 class IndexView(LoginRequiredMixin, generic.ListView):
@@ -16,10 +20,47 @@ class IndexView(LoginRequiredMixin, generic.ListView):
         return Note.objects.filter(owner__pk=user.id)
 
 
-class DetailView(LoginRequiredMixin, generic.DetailView):
+class TagFormView(SingleObjectMixin, generic.FormView):
+
+    form_class = TagForm
+    model = Tag
+
+    def post(self, request, *args, **kwargs):
+        form = request.POST
+        ntag = Tag
+        ntag.name = str(form['name'])
+        # check if the tag already exists
+        # if it exists add tage to note
+        # if does not exist create tag then add it to form
+        ntag.save()
+
+        self.object = self.get_object()
+
+
+
+    def get_success_url(self):
+        redirect('notes:detail', self.object.pk)
+
+
+class NoteView(LoginRequiredMixin, generic.DetailView):
 
     model = Note
     template_name = 'notes/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(NoteView, self).get_context_data(**kwargs)
+        context['form'] = TagForm
+        return context
+
+
+class DetailView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        view = NoteView.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = TagFormView.as_view()
+        return view(request, *args, **kwargs)
 
 
 def new_note(request):
