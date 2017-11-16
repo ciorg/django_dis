e5a0e1eaf74f3ca2e53dd7155468a344c40badf1
ciorg <charlie.iorg@infusionsoft.com>
2017-11-15 21:44:28 -0700

    added home project data

--- dissnitt/mylogin/views.py
+++ dissnitt/mylogin/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from notes.models import Tag, Note
+from todo.models import Project
+
 
 @login_required(login_url='/mylogin/login/')
 def home(request):
-    return render(request, 'mylogin/home.html')
+    projects = Project.objects.all()
+    cprojects = projects.filter(completed=True)
+    cperc = round(float(len(cprojects))/float(len(projects)), 2)
+    notes = Note.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        'projects':projects,
+        'cprojects': cprojects,
+        'cperc': cperc,
+        'notes': notes,
+        'tags': tags
+    }
+
+    return render(request, 'mylogin/home.html', context)
